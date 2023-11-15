59342af519c5e805896710586c97b493cc41faf0
l4rry <l4rryxz@gmail.com>
2023-11-15 08:18:05 +0100

    added GetLoggedOn.py

--- auto-nmap.sh
+++ auto-nmap.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
 target="$1"
-ports=$(nmap -p- --min-rate 1000 "$target" | grep "^ *[0-9]" | grep "open" | cut -d '/' -f 1 | tr '\n' ',' | sed 's/,$//')
+echo "Running first full tcp port scan (-p-) against: $taregt"
+ports=$(nmap -p- --min-rate 1000 "$target" -v -oA full_port_scan | grep "^ *[0-9]" | grep "open" | cut -d '/' -f 1 | tr '\n' ',' | sed 's/,$//')
 
-echo "Running second nmap scan with open ports: $ports"
+echo "Running second nmap service scan (-sC -sV -A) on open ports: $ports"
+nmap -p "$ports" -sC -sV -A "$target" -v -oA service_scan
 
-nmap -p "$ports" -sC -sV -A "$target"
+echo "Running UDP Scan (-sU --top-ports 1000) against: $target"
+nmap -sU --top-ports 1000 "$target" -v -oA udp_top-1000
+
+echo "Running third nmap vuln scan (-sV --script 'vuln') on open ports: $ports"
+nmap -p "$ports" -sV --script "vuln" "$target" -v  -oA vuln_scan
